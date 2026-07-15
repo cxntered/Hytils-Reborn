b35e1dd7e458dc39d2328e02968f267f2b224086
cxntered <me@cxntered.dev>
2026-07-15 17:53:41 +1000

    🐛 fix: use gui mixin for hiding on <=1.21.5
    on 1.21.5 and below, status bars are on the same layer as the hotbar, so we can't remove them properly

--- src/main/kotlin/org/polyfrost/hytils/client/features/game/HideActionBar.kt
+++ src/main/kotlin/org/polyfrost/hytils/client/features/game/HideActionBar.kt
@@ -1,6 +1,8 @@
 package org.polyfrost.hytils.client.features.game
 
+//~ if <1.21.8 'hud.HudElementRegistry' -> 'HudLayerRegistrationCallback'
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry
+//~ if <1.21.8 'hud.VanillaHudElements' -> 'IdentifiedLayer'
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements
 import net.hypixel.data.type.GameType
 import org.polyfrost.hytils.client.HytilsRebornConfig
@@ -8,15 +10,23 @@ import org.polyfrost.oneconfig.api.hypixel.v1.HypixelUtils
 
 object HideActionBar {
     fun init() {
+        //? if >=1.21.8 {
         HudElementRegistry.replaceElement(VanillaHudElements.OVERLAY_MESSAGE) { hudElement ->
+        //?} else
+        //HudLayerRegistrationCallback.EVENT.register { layers ->
             val location = HypixelUtils.getLocation()
             if (!HytilsRebornConfig.isEnabled || !HypixelUtils.isHypixel() || !location.inGame())
+                //~ if <1.21.8 '@replaceElement hudElement' -> '@register'
                 return@replaceElement hudElement
 
             if ((HytilsRebornConfig.hideHousingActionBar && location.gameType.orElse(null) == GameType.HOUSING)
                 || (HytilsRebornConfig.hideDropperActionBar && location.mode.orElse("").contains("DROPPER")))
+                //? if >=1.21.8 {
                 return@replaceElement { _, _ ->  }
+                //?} else
+                //layers.removeLayer(IdentifiedLayer.OVERLAY_MESSAGE)
 
+            //~ if <1.21.8 '@replaceElement hudElement' -> '@register'
             return@replaceElement hudElement
         }
     }
